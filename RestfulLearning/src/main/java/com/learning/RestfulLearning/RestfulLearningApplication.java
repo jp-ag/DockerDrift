3f19648f37faa5bfc3b69e1bfa16220ec2eb2c04
jp <jpaguinsky@gmail.com>
2023-11-14 14:02:49 -0300

    Add: Basic project configuration

---
+++ RestfulLearning/src/main/java/com/learning/RestfulLearning/RestfulLearningApplication.java
@@ -0,0 +1,13 @@
+package com.learning.RestfulLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulLearningApplication.class, args);
+	}
+
+}
---
+++ RestfulLearning/src/test/java/com/learning/RestfulLearning/RestfulLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.RestfulLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
